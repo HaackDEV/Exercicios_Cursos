b1b916eda6998a8a1433aa233a677a620d4eaf8f
Ruan Haack <ruan.haack03@gmail.com>
2025-05-12 21:15:35 -0300

    Primeiro programa em c

---
+++ exercicios_java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 19;
+        String nome = "Ruan";
+        boolean disponibilidade = true;
+        double peso = 81.2;
+        char sexo = 'M';
+
+        System.out.println("Olá!");
+        System.out.println("Seja bem vindo ao primeiro programa em Java!");
+        System.out.println("Meu nome é " +nome+ ", tenho " +idade+ " e peso " +peso+ " kg.");
+
+        
+    }
+}
